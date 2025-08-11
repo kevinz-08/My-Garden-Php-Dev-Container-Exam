b6b032232bb19a31220ed4340a0c3b3044862735
“kevinz-08” <gutierrezamorochokevinsantiago@gmail.com>
2025-08-11 13:49:58 +0000

    feat: :goal_net: Correction in database

--- db/database.sql
+++ db/database.sql
@@ -32,8 +32,31 @@ VALUES (
 
 CREATE TABLE `plantas` (
     `id` int NOT NULL AUTO_INCREMENT,
-    `name` varchar(100) NOT NULL,
-    `family` varchar(100) NOT NULL,
-    `category` ENUM('cactus', 'ornamental', 'frutal') NOT NULL,
+    `nombre` varchar(100) NOT NULL,
+    `familia` varchar(100) NOT NULL,
+    `categoria` ENUM('cactus', 'ornamental', 'frutal') NOT NULL,
+    `proximo_riego` date DEFAULT NULL,
+    `fecha_creacion` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
+);
+
+INSERT INTO plantas
+    (`nombre`, `familia`, `categoria`)
+VALUES
+    ('Aloe Vera', 'Asphodelaceae', 'cactus'),
+    ('Lavanda', 'Lamiaceae', 'ornamental'),
+    ('Fresa', 'Rosaceae', 'frutal'),
+    ('Lengua de suegra', 'Asparagaceae', 'ornamental'),
+    ('Nopal', 'Cactaceae', 'cactus'),
+    ('Tomatera', 'Solanaceae', 'frutal'),
+    ('Orquídea', 'Orchidaceae', 'ornamental'),
+    ('Higuera', 'Moraceae', 'frutal'),
+    ('Sansevieria', 'Asparagaceae', 'ornamental'),
+    ('Pitahaya', 'Cactaceae', 'cactus');
+
+CREATE TABLE `riegos` (
+    `id` int AUTO_INCREMENT PRIMARY KEY,
+    `planta_id` INT NOT NULL,
+    `fecha_riego` DATE NOT NULL,
+    FOREIGN KEY (planta_id) REFERENCES plantas(id) ON DELETE CASCADE
 );
