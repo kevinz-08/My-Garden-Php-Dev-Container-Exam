be084b9f7677c6e9842db84243571d10843cb319
“kevinz-08” <gutierrezamorochokevinsantiago@gmail.com>
2025-08-11 12:49:28 +0000

    feat: :art: Creation of the database

--- db/database.sql
+++ db/database.sql
@@ -1,4 +1,4 @@
--- Active: 1752434449310@@127.0.0.1@3306
+-- Active: 1754396916329@@127.0.0.1@3306@garden
 CREATE DATABASE IF NOT EXISTS `garden`;
 
 USE `garden`;
@@ -29,3 +29,11 @@ VALUES (
         'ana@gmail.com',
         SHA2('h3ll0.', 512)
     );
+
+CREATE TABLE `plantas` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) NOT NULL,
+    `family` varchar(100) NOT NULL,
+    `category` ENUM('cactus', 'ornamental', 'frutal') NOT NULL,
+    PRIMARY KEY (`id`)
+);
